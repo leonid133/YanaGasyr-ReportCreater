f128b7eb29b9feab7a382ed8bde0785800ed5bde
leonid133 <lenin133@yandex.ru>
2015-09-14 03:20:46 +0300

    Чтение из базы по диапазону дат

--- ExcelReportCreater/Form1.cs
+++ ExcelReportCreater/Form1.cs
@@ -140,6 +140,115 @@ namespace ExcelReportCreater
                     
                 }
             }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string MyConString = "Server=localhost;" + "Database=cpp_data;" + "Uid=admin;" + "Pwd=admin;";
+            MySqlConnection connection = new MySqlConnection(MyConString);
+            MySqlCommand cmd;
+            connection.Open();
+
+            try
+            {
+                cmd = connection.CreateCommand();
+                String date_begin = "2015-07-02";
+                String date_end = "2015-07-03";
+                String command_str=  "SELECT * FROM `cpp_data`.`dbm`";
+                       command_str += "where DATE_ADD(`dbm`.`DateTime`, INTERVAL 0 SECOND) > DATE_ADD(\"" + date_begin;
+                       command_str += "\", INTERVAL 0 SECOND)";
+                       command_str += "and DATE_ADD(`dbm`.`DateTime`, INTERVAL 0 SECOND) < DATE_ADD(\"" + date_end;
+                       command_str += "\", INTERVAL 0 SECOND);";
+                cmd.CommandText = command_str;
+                MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adap.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0].DefaultView;
+
+                Excel.Application xlApp;
+                Excel.Workbook xlWorkBook;
+                Excel.Worksheet xlWorkSheet;
+                object misValue = System.Reflection.Missing.Value;
+                Excel.Range chartRange;
+
+                xlApp = new Excel.Application();
+                xlWorkBook = xlApp.Workbooks.Add(misValue);
+                xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+                xlWorkSheet.Cells.DataSeries(ds.Tables[0].DefaultView);
+                /*
+                //add data 
+                xlWorkSheet.Cells[4, 2] = "";
+                xlWorkSheet.Cells[4, 3] = "Student1";
+                xlWorkSheet.Cells[4, 4] = "Student2";
+                xlWorkSheet.Cells[4, 5] = "Student3";
+
+                xlWorkSheet.Cells[5, 2] = "Term1";
+                xlWorkSheet.Cells[5, 3] = "80";
+                xlWorkSheet.Cells[5, 4] = "65";
+                xlWorkSheet.Cells[5, 5] = "45";
+
+                xlWorkSheet.Cells[6, 2] = "Term2";
+                xlWorkSheet.Cells[6, 3] = "78";
+                xlWorkSheet.Cells[6, 4] = "72";
+                xlWorkSheet.Cells[6, 5] = "60";
+
+                xlWorkSheet.Cells[7, 2] = "Term3";
+                xlWorkSheet.Cells[7, 3] = "82";
+                xlWorkSheet.Cells[7, 4] = "80";
+                xlWorkSheet.Cells[7, 5] = "65";
+
+                xlWorkSheet.Cells[8, 2] = "Term4";
+                xlWorkSheet.Cells[8, 3] = "75";
+                xlWorkSheet.Cells[8, 4] = "82";
+                xlWorkSheet.Cells[8, 5] = "68";
+
+                xlWorkSheet.Cells[9, 2] = "Total";
+                xlWorkSheet.Cells[9, 3] = "315";
+                xlWorkSheet.Cells[9, 4] = "299";
+                xlWorkSheet.Cells[9, 5] = "238";
+                */
+                xlWorkSheet.get_Range("b2", "e3").Merge(false);
+
+                chartRange = xlWorkSheet.get_Range("b2", "e3");
+                chartRange.FormulaR1C1 = "MARK LIST";
+                chartRange.HorizontalAlignment = 3;
+                chartRange.VerticalAlignment = 3;
+                chartRange.Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Yellow);
+                chartRange.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red);
+                chartRange.Font.Size = 20;
+
+                chartRange = xlWorkSheet.get_Range("b4", "e4");
+                chartRange.Font.Bold = true;
+                chartRange = xlWorkSheet.get_Range("b9", "e9");
+                chartRange.Font.Bold = true;
+
+                chartRange = xlWorkSheet.get_Range("b2", "e9");
+                chartRange.BorderAround(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlMedium, Excel.XlColorIndex.xlColorIndexAutomatic, Excel.XlColorIndex.xlColorIndexAutomatic);
+
+                xlWorkBook.SaveAs("d:\\17.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                xlWorkBook.Close(true, misValue, misValue);
+                xlApp.Quit();
+
+                releaseObject(xlApp);
+                releaseObject(xlWorkBook);
+                releaseObject(xlWorkSheet);
+
+                MessageBox.Show("File created !");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Didn't Connect");
+            }
+            finally
+            {
+
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+
+                }
+            }
         } 
     }
 }
