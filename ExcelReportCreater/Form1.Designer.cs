9badd5c4ccf6ff0065af2ff83411550388b7cc57
Leonid133 <lenin133@yandex.ru>
2015-09-15 00:23:27 +0300

    Выбор из календаря Выгрузка по шаблону в Эксель

--- ExcelReportCreater/Form1.Designer.cs
+++ ExcelReportCreater/Form1.Designer.cs
@@ -34,6 +34,9 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -60,16 +63,16 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(98, 86);
+            this.dataGridView1.Location = new System.Drawing.Point(3, 278);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(859, 347);
+            this.dataGridView1.Size = new System.Drawing.Size(1004, 347);
             this.dataGridView1.TabIndex = 2;
             // 
             // button3
             // 
             this.button3.Location = new System.Drawing.Point(196, 13);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(139, 40);
             this.button3.TabIndex = 3;
             this.button3.Text = "прил № 17";
             this.button3.UseVisualStyleBackColor = true;
@@ -77,27 +80,53 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(291, 13);
+            this.button4.Location = new System.Drawing.Point(196, 59);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(139, 42);
             this.button4.TabIndex = 4;
             this.button4.Text = "прил №18";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(196, 43);
+            this.button5.Location = new System.Drawing.Point(196, 107);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(139, 38);
             this.button5.TabIndex = 5;
             this.button5.Text = "прил №19";
             this.button5.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(420, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            this.dateTimePicker1.Value = new System.DateTime(2015, 9, 14, 0, 0, 0, 0);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(420, 75);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 7;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(420, 46);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1035, 504);
+            this.ClientSize = new System.Drawing.Size(1035, 640);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -119,6 +148,9 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
     }
 }
 
--- ExcelReportCreater/Form1.cs
+++ ExcelReportCreater/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop;
+using System.Runtime.InteropServices;
 using MySql.Data.MySqlClient;
 
 
@@ -23,74 +25,7 @@ namespace ExcelReportCreater
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Excel.Application xlApp;
-            Excel.Workbook xlWorkBook;
-            Excel.Worksheet xlWorkSheet;
-            object misValue = System.Reflection.Missing.Value;
-            Excel.Range chartRange;
-
-            xlApp = new Excel.Application();
-            xlWorkBook = xlApp.Workbooks.Add(misValue);
-            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-
-            //add data 
-            xlWorkSheet.Cells[4, 2] = "";
-            xlWorkSheet.Cells[4, 3] = "Student1";
-            xlWorkSheet.Cells[4, 4] = "Student2";
-            xlWorkSheet.Cells[4, 5] = "Student3";
-
-            xlWorkSheet.Cells[5, 2] = "Term1";
-            xlWorkSheet.Cells[5, 3] = "80";
-            xlWorkSheet.Cells[5, 4] = "65";
-            xlWorkSheet.Cells[5, 5] = "45";
-
-            xlWorkSheet.Cells[6, 2] = "Term2";
-            xlWorkSheet.Cells[6, 3] = "78";
-            xlWorkSheet.Cells[6, 4] = "72";
-            xlWorkSheet.Cells[6, 5] = "60";
-
-            xlWorkSheet.Cells[7, 2] = "Term3";
-            xlWorkSheet.Cells[7, 3] = "82";
-            xlWorkSheet.Cells[7, 4] = "80";
-            xlWorkSheet.Cells[7, 5] = "65";
-
-            xlWorkSheet.Cells[8, 2] = "Term4";
-            xlWorkSheet.Cells[8, 3] = "75";
-            xlWorkSheet.Cells[8, 4] = "82";
-            xlWorkSheet.Cells[8, 5] = "68";
-
-            xlWorkSheet.Cells[9, 2] = "Total";
-            xlWorkSheet.Cells[9, 3] = "315";
-            xlWorkSheet.Cells[9, 4] = "299";
-            xlWorkSheet.Cells[9, 5] = "238";
-
-            xlWorkSheet.get_Range("b2", "e3").Merge(false);
-
-            chartRange = xlWorkSheet.get_Range("b2", "e3");
-            chartRange.FormulaR1C1 = "MARK LIST";
-            chartRange.HorizontalAlignment = 3;
-            chartRange.VerticalAlignment = 3;
-            chartRange.Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Yellow);
-            chartRange.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red);
-            chartRange.Font.Size = 20;
-
-            chartRange = xlWorkSheet.get_Range("b4", "e4");
-            chartRange.Font.Bold = true;
-            chartRange = xlWorkSheet.get_Range("b9", "e9");
-            chartRange.Font.Bold = true;
-
-            chartRange = xlWorkSheet.get_Range("b2", "e9");
-            chartRange.BorderAround(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlMedium, Excel.XlColorIndex.xlColorIndexAutomatic, Excel.XlColorIndex.xlColorIndexAutomatic);
-
-            xlWorkBook.SaveAs("d:\\csharp.net-informations.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
-            xlWorkBook.Close(true, misValue, misValue);
-            xlApp.Quit();
-
-            releaseObject(xlApp);
-            releaseObject(xlWorkBook);
-            releaseObject(xlWorkSheet);
-
-            MessageBox.Show("File created !");
+            
         }
 
         private void releaseObject(object obj)
@@ -141,6 +76,44 @@ namespace ExcelReportCreater
                 }
             }
         }
+   
+        public void ExportToExcel_17(DataGridView grid)
+        {
+ 
+            Excel.Application Exl = new Excel.Application();
+            Excel.Workbook wb;
+
+            Excel.XlReferenceStyle RefStyle = Exl.ReferenceStyle;
+            Exl.Visible = true;
+            
+            String TemplatePath = System.Windows.Forms.Application.StartupPath + @"\Экспорт данных_17.xltx";
+            try
+            {
+                wb = Exl.Workbooks.Add(TemplatePath); // !!! 
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception("Не удалось загрузить шаблон для экспорта " + TemplatePath + "\n" + ex.Message);
+            }
+            Excel.Worksheet ws = wb.Worksheets.get_Item(1) as Excel.Worksheet;
+            int startCells = 13;
+            for (int j = 0; j < grid.Columns.Count; ++j)
+            {
+                (ws.Cells[startCells, j + 1] as Excel.Range).Value2 = grid.Columns[j].HeaderText;
+                for (int i = startCells; i < grid.Rows.Count; ++i)
+                {
+                    object Val = grid.Rows[i-startCells].Cells[j].Value;
+                    if (Val != null)
+                        (ws.Cells[i + 2, j + 1] as Excel.Range).Value2 = Val.ToString();
+                }
+            }
+            ws.Columns.EntireColumn.AutoFit();
+            Exl.ReferenceStyle = RefStyle;
+            releaseObject(Exl as Object);
+
+                MessageBox.Show("File created !");
+            
+        }
 
         private void button3_Click(object sender, EventArgs e)
         {
@@ -154,87 +127,19 @@ namespace ExcelReportCreater
                 cmd = connection.CreateCommand();
                 String date_begin = "2015-07-02";
                 String date_end = "2015-07-03";
+                date_begin = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+                date_end = dateTimePicker2.Value.ToString("yyyy-MM-dd");
                 String command_str=  "SELECT * FROM `cpp_data`.`dbm`";
                        command_str += "where DATE_ADD(`dbm`.`DateTime`, INTERVAL 0 SECOND) > DATE_ADD(\"" + date_begin;
                        command_str += "\", INTERVAL 0 SECOND)";
                        command_str += "and DATE_ADD(`dbm`.`DateTime`, INTERVAL 0 SECOND) < DATE_ADD(\"" + date_end;
-                       command_str += "\", INTERVAL 0 SECOND);";
+                       command_str += "\", INTERVAL 86400 SECOND);";
                 cmd.CommandText = command_str;
                 MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
                 DataSet ds = new DataSet();
                 adap.Fill(ds);
                 dataGridView1.DataSource = ds.Tables[0].DefaultView;
-
-                Excel.Application xlApp;
-                Excel.Workbook xlWorkBook;
-                Excel.Worksheet xlWorkSheet;
-                object misValue = System.Reflection.Missing.Value;
-                Excel.Range chartRange;
-
-                xlApp = new Excel.Application();
-                xlWorkBook = xlApp.Workbooks.Add(misValue);
-                xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-
-                xlWorkSheet.Cells.DataSeries(ds.Tables[0].DefaultView);
-                /*
-                //add data 
-                xlWorkSheet.Cells[4, 2] = "";
-                xlWorkSheet.Cells[4, 3] = "Student1";
-                xlWorkSheet.Cells[4, 4] = "Student2";
-                xlWorkSheet.Cells[4, 5] = "Student3";
-
-                xlWorkSheet.Cells[5, 2] = "Term1";
-                xlWorkSheet.Cells[5, 3] = "80";
-                xlWorkSheet.Cells[5, 4] = "65";
-                xlWorkSheet.Cells[5, 5] = "45";
-
-                xlWorkSheet.Cells[6, 2] = "Term2";
-                xlWorkSheet.Cells[6, 3] = "78";
-                xlWorkSheet.Cells[6, 4] = "72";
-                xlWorkSheet.Cells[6, 5] = "60";
-
-                xlWorkSheet.Cells[7, 2] = "Term3";
-                xlWorkSheet.Cells[7, 3] = "82";
-                xlWorkSheet.Cells[7, 4] = "80";
-                xlWorkSheet.Cells[7, 5] = "65";
-
-                xlWorkSheet.Cells[8, 2] = "Term4";
-                xlWorkSheet.Cells[8, 3] = "75";
-                xlWorkSheet.Cells[8, 4] = "82";
-                xlWorkSheet.Cells[8, 5] = "68";
-
-                xlWorkSheet.Cells[9, 2] = "Total";
-                xlWorkSheet.Cells[9, 3] = "315";
-                xlWorkSheet.Cells[9, 4] = "299";
-                xlWorkSheet.Cells[9, 5] = "238";
-                */
-                xlWorkSheet.get_Range("b2", "e3").Merge(false);
-
-                chartRange = xlWorkSheet.get_Range("b2", "e3");
-                chartRange.FormulaR1C1 = "MARK LIST";
-                chartRange.HorizontalAlignment = 3;
-                chartRange.VerticalAlignment = 3;
-                chartRange.Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Yellow);
-                chartRange.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red);
-                chartRange.Font.Size = 20;
-
-                chartRange = xlWorkSheet.get_Range("b4", "e4");
-                chartRange.Font.Bold = true;
-                chartRange = xlWorkSheet.get_Range("b9", "e9");
-                chartRange.Font.Bold = true;
-
-                chartRange = xlWorkSheet.get_Range("b2", "e9");
-                chartRange.BorderAround(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlMedium, Excel.XlColorIndex.xlColorIndexAutomatic, Excel.XlColorIndex.xlColorIndexAutomatic);
-
-                xlWorkBook.SaveAs("d:\\17.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
-                xlWorkBook.Close(true, misValue, misValue);
-                xlApp.Quit();
-
-                releaseObject(xlApp);
-                releaseObject(xlWorkBook);
-                releaseObject(xlWorkSheet);
-
-                MessageBox.Show("File created !");
+                ExportToExcel_17(dataGridView1);
             }
             catch (Exception)
             {
